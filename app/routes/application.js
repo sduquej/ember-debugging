8cd69f84f4cad09fca229d6f041e26c8785b2343
Sebastian Duque <sduquej@gmail.com>
2018-03-21 17:00:35 -0500

    oops

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return fetch('https://api.chucknorris.io/jokes/categories').then(response => response.json());
+    return fetch('https://api.chucknorris.io/jokes/categories');
   }
 });
